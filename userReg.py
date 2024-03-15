f2396c5ab0975fa00177afbbb34cffebf3642e1c
mastura <64648501+yoongzbear@users.noreply.github.com>
2024-03-15 12:26:15 +0800

    added registration function
    Insert user's registration details into database

--- userReg.py
+++ userReg.py
@@ -1,2 +1,26 @@
 import pymysql
-conn = pymysql.connect(user='root', password='', host='localhost', database='accountpy'
+#connection to mysql database
+conn = pymysql.connect(user='root', password='', host='localhost', database='accountpy')
+cursor = conn.cursor()
+
+#function to insert user details into database
+def insertUser(username, password, name):
+    insertUser = "INSERT INTO user (username, password, name) VALUES (%s, %s, %s);"
+    data = (username, password, name)
+    cursor.execute(insertUser, data)
+    conn.commit()
+    print("User registered successfully! \nWelcome " + name + "!!")
+    choice = input("Would you like to login? (Y/N): ")
+    if choice.upper() == "Y":
+        login() #login function
+    else:
+        print("Thank you for registering with us. Have a nice day!!")
+        conn.close()
+
+print ("Account Registration")
+print ("Enter your details below: ")
+username = input("Username: ")
+password = input("Password: ")
+name = input("Name: ")
+
+insertUser(username, password, name)
