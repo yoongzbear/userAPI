0fa9b78f5abe8142f03558a09a9788ea3a7bd490
mastura <64648501+yoongzbear@users.noreply.github.com>
2024-03-17 00:22:12 +0800

    validation
    Implemented data validation

--- userReg.py
+++ userReg.py
@@ -23,6 +23,7 @@ def logout():
         print("Okay, continue with your session.")
         choice = input("Would you like to logout? (Y/N): ")
     print("Thank you for visiting AccountPy. Have a nice day!")
+    conn.close()
 
 def login():
     print("-----Login-----")
@@ -44,21 +45,51 @@ def register():
     print ("-----Registration-----")
     print ("Enter your details below: ")
     username = input("Username: ")
+    #check if username already exists
+    while True:
+        check = "SELECT * FROM user WHERE username = %s;"
+        cursor.execute(check, username)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            print("Username already exists. Please try another username.")
+            username = input("Username: ")
+        else:
+            break
+    
     password = input("Password: ")
+    #password length validation 
+    while len(password) < 8:
+        print("Password must be at least 8 characters long.")
+        password = input("Password: ")
+    #password confirmation
+    passwordConfirm = input("Confirm password: ")
+    while password != passwordConfirm:
+        print("Passwords do not match. Please try again.")
+        password = input("Password: ")
+        passwordConfirm = input("Confirm password: ")
+
     name = input("Name: ")
     #call insert user function 
-    insertUser(username, password, name)
+    insertUser(username, passwordConfirm, name.title())
 
 #main
-print("Welcome to AccountPy! \nPlease enter your choice for next action")
-print("1. Register")
-print("2. Login")
-print("3. Exit")
-choice = input("Enter your choice: ")
-if choice == "1":
-    register()
-elif choice == "2":
-    login()
-else:
-    print("Thank you for visiting AccountPy. Have a nice day!")
-    conn.close()
+print("Welcome to AccountPy!")
+while True:
+    print ("-----Main Menu-----")
+    print ("Please select an option below: ")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        register()
+        break
+    elif choice == 2:
+        login()
+        break
+    elif choice == 3:
+        print("Thank you for visiting AccountPy. Have a nice day!")
+        conn.close()
+        break
+    else:
+        print("Invalid choice. Please enter choice 1-3.")
